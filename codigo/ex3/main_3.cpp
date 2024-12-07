0b1e3fd1d228b929261624e774a562a3c6645cd8
eliabe36i <eliabe36i@gmail.com>
2024-12-07 00:18:14 -0300

    ex3

---
+++ codigo/ex3/main_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+int f1;
+int f2;
+int f3;
+vet[10];
+
+for (int i=0;i<10;i++){
+	cout<<"Digite os aluno 1 que faltou"<<endl;
+	cin>>f1;
+	cout<<"Digite os alunos 2 que faltou"<<endl;
+	cin>>f2;
+	cout<<"Digite o alunos 3 que faltou"<<endl;
+	cin>>f3;
+if (i==f1){
+	cout<<"o aluno da matricula "<<i<<"faltou"<<endl;
+	
+}	
+if else (i==f2){
+	cout<<"o alunon da mmatricula "<<i<<"faltou<<"<<endl;
+}
+if else (i==f3){
+	cout<<"o aluno da matricula "<<i<<"faltou"<<endl;
+}
+}
+}
+	return 0;
+}
---
+++ codigo/ex3/main_new.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+int resp;
+int vet[10];
+
+
+
+
+for (int i=0;i<10;i++){
+	switch (i){
+		case 0:
+			cout<<"matricula 1 faltou :"<<endl;
+			cout<<"-------------------------"<<endl;
+			break;
+		case 1:
+			cout<<"matricula 2 faltou"<<endl;
+			cout<<"------------------"<<endl;
+			break;
+		case 2:
+			cout<<"matricula 3 presente"<<endl;	
+			cout<<"-------------------"<<endl;
+				break;
+		case 3:
+			cout<<"matricula 4 presente "<<endl;
+			cout<<"-------------------"<<endl;
+				break;
+		case 4 :
+			cout<<"matricula 5 presente"<<endl;
+			cout<<"---------------------"<<endl;
+				break;	
+		case 5:
+			cout<<"matricula 6 presente"<<endl;
+			cout<<"---------------"<<endl;
+				break;
+		case 6:
+			cout<<"Matricula 7 presente"<<endl;
+			cout<<"-------------------"<<endl;
+				break;
+		case 7:
+			cout<<"Matricula 8 presente"<<endl;
+			cout<<"--------------------------"<<endl;
+				break;									
+		case 8:
+			cout<<"matricula 9 faltou-- Favor comunicar cordenador "<<endl;
+			cout<<"-----------------------"<<endl;
+				break;
+		case 9:
+			cout<<"matricula 10 presente"<<endl;
+				break;
+								
+	}
+}
+	return 0;
+}
