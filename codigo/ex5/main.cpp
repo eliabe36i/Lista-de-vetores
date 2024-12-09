f80d66db296a458f937aeda4e7decf5a914129ee
eliabe36i <eliabe36i@gmail.com>
2024-12-08 21:13:18 -0300

    ex5

--- codigo/ex5/main.cpp
+++ codigo/ex5/main.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 int main() {
 	
-int vet[5];	
-int vetor[5];
+int v[5];	
+int t[5];
+int maior =0;
 
+	
 	for(int i=0;i<5;i++){
-		cout<<"Digite os numeros dos vetores "<<endl;
-			cin>>vet[i];
-				for(int j=0;i<5;i++){
-					vet[j]=vet[i];
-				}
-	}
-	
-	
+		
+		cout<<"Digite o  numero do valor do vetor"<<endl;
+		cin>>v[i];
+			if(v[i]>maior){
+				maior=v[i];
+		}}
+		for (int i=0;i<5;i++){
+			t[i]=v[i]*maior;
+		cout<<t[i]<<endl;
+		}
 	
 	return 0;
 }
